111c2df71f7676ba8c8eb4b6ecf37b2030d60ca6
Jay Park <88129753+JayPark-UTEP@users.noreply.github.com>
2022-07-20 19:32:35 -0600

    Add files via upload

---
+++ JavaScript/JavaScriptStudy/index.js
@@ -0,0 +1,39 @@
+// let name = "jay";
+// let age = 27;
+// let person = {
+//   name: "Jay",
+//   age: 27
+// }
+
+// person.name = "John";
+
+// let selection = "name";
+
+// person[selection]="Mary";
+
+// console.log(person.name);
+
+//Array=====================================
+// let selectedColors = ["red", "blue"];
+// selectedColors[2] = "green";
+// selectedColors[3] = 10;
+
+// console.log(selectedColors.length);
+
+//Function==================================
+
+// function great(name, lastName) {
+//   console.log("Hello " + name + " " + lastName);
+// }
+
+// great("Jay", "Park");
+// great("Nick"); // lastName undefinded
+
+//Type of function==========================
+function square(number) {
+  return number * number;
+}
+
+let number = square(2);
+
+console.log(number);
